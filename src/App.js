55c13b819171a59a6f3952a16f775c800ea78744
fakrul01 <fislamc6@gmail.com>
2023-02-12 01:18:40 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Random Password Generator</h1>
+      <p>This tool can generate secure, strong, random passwords. To ensure security, the password is generated completely on the webpage without being sent across the Internet.</p>
+      <li>Password Length:</li>
+      <form action="">
+        <ul>
+
+        </ul>
+
+      </form>
     </div>
   );
 }
---
+++ src/components/Password.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Password.css';
+const Password = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default Password;
